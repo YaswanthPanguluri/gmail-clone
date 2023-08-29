a0ec69aecbaa0ced6f8ac3a0183202b5c40f2664
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-08-28 20:08:37 -0500

    time 38:10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hai yaswanth</h2>
+      <Header />
+      <h2>Hai yaswanth panguluri</h2>
     </div>
   );
 }
